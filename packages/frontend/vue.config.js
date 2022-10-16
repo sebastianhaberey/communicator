84c34544ce3a9b629a6af14a5a24fa3a063415a5
Sebastian Haberey <sebastian@haberey.com>
2022-10-16 19:15:03 +0200

    updated dependencies

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-undef */
+// const { defineConfig } = require("@vue/cli-service");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
   lintOnSave: false,
   transpileDependencies: ["vuetify", "vuex-module-decorators"],
   configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
     performance: {
       hints: false,
     },
